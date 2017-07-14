474473711ec1dff558e49e1135baa06de4931529
Chen Yu <chenyu.nus@gmail.com>
2017-07-14 21:02:02 +0800

    added dice

--- app.elm
+++ app.elm
@@ -1,22 +1,27 @@
 module Main exposing (..)
 
-import Html exposing (Html, button, div, text)
-import Html.Attributes exposing (style)
+import Html exposing (Html, body, button, div, text)
+import Html.Attributes exposing (style, class)
 import Html.Events exposing (onClick)
-
-
-main =
-    Html.beginnerProgram { model = model, view = view, update = update }
-
+import Tuple
+import Random
+import Mouse
+import Keyboard
 
 
 -- MODEL
+
+
+type DiceFace
+    = Unset
+    | SetDiceFace Int
 
 
 type alias Model =
     { count : Int
     , color : String
     , defaultColor : String
+    , diceFaces : { left : DiceFace, right : DiceFace }
     }
 
 
@@ -25,11 +30,12 @@ model =
     { count = 0
     , color = "black"
     , defaultColor = "black"
+    , diceFaces = { left = Unset, right = Unset }
     }
 
 
 
--- UPDATE
+-- MESSAGES
 
 
 type Msg
@@ -37,22 +43,67 @@ type Msg
     | Decrement
     | Reset
     | SetColor String
+    | RollDices
+    | UpdateFaces ( Int, Int )
+    | MouseMsg Mouse.Position
+    | KeyMsg Keyboard.KeyCode
 
 
-update : Msg -> Model -> Model
+
+-- UPDATE
+
+
+randFaceIntGen =
+    Random.int 1 6
+
+
+randFacesIntGen =
+    Random.pair randFaceIntGen randFaceIntGen
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        Increment ->
-            { model | count = model.count + 1 }
+    let
+        newModel =
+            case msg of
+                Increment ->
+                    { model | count = model.count + 1 }
 
-        Decrement ->
-            { model | count = model.count - 1 }
+                Decrement ->
+                    { model | count = model.count - 1 }
 
-        Reset ->
-            { model | count = 0, color = model.defaultColor }
+                Reset ->
+                    { model | count = 0, color = model.defaultColor }
 
-        SetColor string ->
-            { model | color = string }
+                SetColor string ->
+                    { model | color = string }
+
+                RollDices ->
+                    model
+
+                UpdateFaces ( valL, valR ) ->
+                    { model
+                        | diceFaces =
+                            { left = SetDiceFace valL
+                            , right = SetDiceFace valR
+                            }
+                    }
+
+                MouseMsg pos ->
+                    model
+
+                KeyMsg code ->
+                    model
+
+        command =
+            case msg of
+                RollDices ->
+                    Random.generate UpdateFaces randFacesIntGen
+
+                others ->
+                    Cmd.none
+    in
+        ( newModel, command )
 
 
 
@@ -61,11 +112,71 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [ style [ ( "color", model.color ) ] ] [ text (toString model.count) ]
-        , button [ onClick Increment ] [ text "+" ]
-        , button [ onClick Reset ] [ text "Reset" ]
-        , button [ onClick (SetColor "red") ] [ text "Red" ]
-        , button [ onClick (SetColor "blue") ] [ text "Blue" ]
+    body []
+        [ div []
+            [ button [ onClick Decrement ] [ text "-" ]
+            , div [ style [ ( "color", model.color ) ] ]
+                [ model.count |> toString |> text ]
+            , button [ onClick Increment ] [ text "+" ]
+            , button [ onClick Reset ] [ text "Reset" ]
+            , button [ onClick (SetColor "red") ] [ text "Red" ]
+            , button [ onClick (SetColor "blue") ] [ text "Blue" ]
+            ]
+        , div []
+            [ viewDice model.diceFaces.left
+            , viewDice model.diceFaces.right
+            ]
+        , button [ onClick RollDices ] [ text "Roll" ]
         ]
+
+
+viewDice : DiceFace -> Html Msg
+viewDice face =
+    div
+        [ style
+            [ ( "border", "solid" )
+            , ( "width", "1em" )
+            ]
+        , class "dice"
+        ]
+        [ case face of
+            SetDiceFace faceInt ->
+                faceInt |> toString |> text
+
+            Unset ->
+                "?" |> text
+        ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ Mouse.clicks MouseMsg
+        , Keyboard.downs KeyMsg
+        ]
+
+
+
+-- INIT
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( model, Cmd.none )
+
+
+
+-- MAIN
+
+
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
